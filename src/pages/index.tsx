f4fe24a3352be9f5ef568158e3934e67fa888f2b
Ajay Titus <ajaytitus1386@gmail.com>
2023-05-23 19:04:28 +0530

    chore: rmv unused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image"
 import { Inter } from "next/font/google"
-import DicePicker from "../components/dice/DicePicker"
-import DiceList from "../components/dice/DiceList"
-import ChatWindow from "../components/chat/ChatWindow"
 import RoomDialogBox from "../components/index/RoomDialogBox"
-import DiceTable from "../components/room/DiceTable"
 import Hero from "../components/index/Hero"
 
 const inter = Inter({ subsets: ["latin"] })
