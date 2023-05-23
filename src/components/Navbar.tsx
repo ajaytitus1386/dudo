2b98e5fa7387db23baa386ecac766a3c6df3bc30
Ajay Titus <ajaytitus1386@gmail.com>
2023-05-23 18:58:39 +0530

    feat: add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <div className="w-full flex items-center justify-between fixed z-10 top-0 px-4 py-2 bg-background-light shadow-md">
+            <h1 className="text-2xl font-bold">Liars Dice</h1>
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default Navbar
