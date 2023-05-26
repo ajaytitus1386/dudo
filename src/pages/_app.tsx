7e163bf6612cd050085dfa55bd791a1ab5106e28
Ajay Titus <ajaytitus1386@gmail.com>
2023-05-26 10:00:04 +0530

    chore: add chevron icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faPaperPlane, faArrowsRotate } from "@fortawesome/free-solid-svg-icons"
+import {
+    faPaperPlane,
+    faArrowsRotate,
+    faChevronDown,
+} from "@fortawesome/free-solid-svg-icons"
 import Layout from "../components/Layout"
 
-library.add(faPaperPlane, faArrowsRotate)
+library.add(faPaperPlane, faArrowsRotate, faChevronDown)
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
