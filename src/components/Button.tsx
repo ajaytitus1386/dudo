1c6b591a20eb3b2d595b814c86694fd67586460c
Ajay Titus <ajaytitus1386@gmail.com>
2023-06-08 15:00:17 +0530

    style: button width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<Props> = ({ className, onClick, children }) => {
     return (
         <button
             className={[
-                "w-16 h-8 bg-blue-400 rounded-md hover:bg-blue-600",
+                "w-auto min-w-[4rem] h-8 bg-blue-400 rounded-md hover:bg-blue-600 text-white",
                 className,
             ].join(" ")}
             onClick={onClick}
