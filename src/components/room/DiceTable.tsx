7089db4fbaa445bf817959715d355a28c07b540a
Ajay Titus <ajaytitus1386@gmail.com>
2023-07-08 01:52:45 +0530

    style: current user ping

--- src/components/room/DiceTable.tsx
+++ src/components/room/DiceTable.tsx
@@ -92,11 +92,14 @@ const PlayerHand = ({
     return (
         <>
             <div
-                className={`m-auto text-md text-text-light-500 dark:text-text-dark-500 col-span-2 rounded-md p-1 ${
-                    isActive && "border-b-2 border-green-400 rounded-b-none"
-                }`}
+                className={`relative m-auto text-md text-text-light-500 dark:text-text-dark-500 col-span-2 p-1`}
             >
                 <Username username={playerName} />
+                {isActive && (
+                    <div
+                        className={`absolute z-10 -left-2 top-1/2 -translate-y-1/2 w-2 h-2 rounded-full bg-green-400 animate-pulse`}
+                    />
+                )}
             </div>
             {Array.from(Array(maxDice).keys()).map((i) => {
                 // Known Die
