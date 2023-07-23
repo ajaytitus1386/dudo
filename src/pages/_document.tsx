4bf4f146bbac7e3ddfa6aef5e9f7bfbf7809797a
Ajay Titus <ajaytitus1386@gmail.com>
2023-07-23 16:30:57 +0530

    feat: add page metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,18 @@ export default function Document() {
             <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
             <link rel="icon" type="image/png" href="/favicon.png" />
             <title>Liar&apos;s Dice</title>
+            <meta
+                name="description"
+                content="A simple website to play Liar's Dice with your friends online."
+            />
+            <meta property="og:title" content="Liar's Dice" />
+            <meta
+                property="og:description"
+                content="A simple website to play Liar's Dice with your friends online."
+            />
+            <meta property="og:image" content="/favicon.png" />
+            <meta property="og:url" content="https://dudo.ajaytitus.com" />
+            <meta property="og:type" content="website" />
             <body>
                 <Main />
                 <NextScript />
